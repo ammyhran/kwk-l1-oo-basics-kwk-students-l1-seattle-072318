3498c5565fbce9e73454fedd28a0e2066160c142
ammyhran <ammyhran@gmail.com>
2018-07-26 23:08:55 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
 # book.rb
 # Add you Book class here
+
+class Book
+  attr_accessor :author, :page_count, :assert_generates
+  attr_reader :title 
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def turn_page
+    puts "FLipping the page...wow, you read fast!"
+  end
+  
+end
+
+book_one = Book.new("Harry Potter")
+
+puts book_one.title
